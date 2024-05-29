5e3465f199505b68db18bbac76034df05666c46b
Jack <jackrgisel@gmail.com>
2024-05-29 16:55:39 +0200

    deletes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit"
 export default defineConfig({
     dialect: "sqlite",
     driver: "turso",
-    schema: "./db/schema.ts",
+    schema: "./db/schema.js",
     out: "./drizzle",
     dbCredentials: {
         url: process.env.DATABASE_URL,
