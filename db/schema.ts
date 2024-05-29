e4af2cbea618ed4887b3fd603d7e69f4ce9f0e55
Jack <jackrgisel@gmail.com>
2024-05-29 13:41:05 +0200

    added user table

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,16 @@
-import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core'
-import {sql} from "drizzle-orm"
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
+import { sql } from "drizzle-orm"
 
-export const actions = sqliteTable('actions', {
-    id: integer('id').primaryKey(),
-    title: text('title'),
-    prompt: text('prompt'),
-    createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`).notNull()
+export const actions = sqliteTable("actions", {
+  id: integer("id").primaryKey(),
+  title: text("title"),
+  prompt: text("prompt"),
+  createdAt: text("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+})
+
+export const users = sqliteTable("actions", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
 })
